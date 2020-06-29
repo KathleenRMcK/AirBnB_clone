92f76bcee1391fb7befb4558d2bfe1cb77f81026
dt <dtvangogh@github.com>
2020-06-28 21:02:33 -0400

    initial console with help command, EOF, exit

---
+++ console.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""
+This module contains the command interpeter
+for managing Airbnb files
+"""
+import cmd
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models import storage, allclasses
+from datetime import datetime
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import shlex
+import re
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Class that inherits from cmd.Cmd
+    """
+    prompt = '(airbnb) '
+
+    def do_EOF(self, args):
+        """
+        EOF command exits out of the command interpreter
+        """
+        quit()
+    def do_help(self, args):
+        """
+        Command lists all help details for each command
+        """
+        cmd.Cmd.do_help(self, args)
+
+
+    def do_destroy(self, args):
+        """
+        Deletes an instance based on the class name and id
+        saves the changes into JSON file
+        """
+
+    def do_all(self, args):
+        """
+        Prints all string representation of all instances
+        based or not on the class name
+        """
+    def do_count(self, args):
+        """
+        Counts number of instances of a class
+        """
+
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it to JSON file
+        and prints the id
+        """
+
+    def do_update(self, args):
+        """
+        Update an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file).
+        """
+
+
+    def do_show(self, args):
+        """ show string representation of an instance"""
+
+
+    def default(self, args):
+        """
+        default method to use with command()
+        """
+
+
+    def do_quit(self, args):
+        """
+        Quit command exits out of the command interpreter
+        """
+        quit()
+
+
+    def emptyline(self):
+        """
+        Returns back to the prompt
+        """
+        return
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
