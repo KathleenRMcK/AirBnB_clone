92e61dccbe9ce76198551eeab82edd9bea235108
dtvangogh <dt.van16@gmail.com>
2020-06-30 00:15:24 -0400

    add destroy, create, show

--- console.py
+++ console.py
@@ -19,7 +19,7 @@ class HBNBCommand(cmd.Cmd):
     """
     Class that inherits from cmd.Cmd
     """
-    prompt = '(airbnb) '
+    prompt = '(hbnb) '
 
     def do_EOF(self, args):
         """
@@ -38,6 +38,55 @@ class HBNBCommand(cmd.Cmd):
         Deletes an instance of the class name and id
         saves the changes inside JSON file
         """
+        tokens = args.split()
+        if tokens[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(tokens) < 2:
+            print("** instance id missing **")
+            return
+        if len(tokens) < 1:
+            print("** class name missing **")
+            return
+        key = tokens[0] + "." + tokens[1]
+        if key in BaseModelStorage:
+            BaseModelStorage.all().pop(key)
+            """ use .save """
+            return
+        else:
+            print("** no instance found **")
+
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel and saves it inside JSON flie
+        """
+        tokens = args.split()
+        if len(tokens) < 1:
+            print("** class name missing **")
+            return
+        if tokens[0] in HBNBCommand.classes:
+            if tokens[0] in HBNBCommand.classes:
+                if tokens[0] == "BaseModel":
+                    create_model = BaseModel()
+                if tokens[0] == "User":
+                    create__model = User()
+                if tokens[0] == "State":
+                    create__model = State()
+                if tokens[0] == "Amenity":
+                    create_model = Amenity()
+                if tokens[0] == "Review":
+                    create_model = Review()
+                if tokens[0] == "Place":
+                    create_model = Place()
+                if tokens[0] == "City":
+                    create_model = City()
+                model.save()
+                print(create_model.id)
+
+        else:
+            print("** class doesn't exist **")
+
+
 
     def do_all(self, args):
         """
@@ -48,11 +97,6 @@ class HBNBCommand(cmd.Cmd):
         Counts number of instances
         """
 
-    def do_create(self, args):
-        """
-        Creates a new instance of BaseModel and saves it inside JSON file
-
-        """
 
     def do_update(self, args):
         """
@@ -63,6 +107,23 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, args):
         """ show string representation of an instance"""
+        tokens = args.split()
+        if tokens[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(tokens) < 2:
+            if len(args[0]) != 36:
+                print("** instance id missing **")
+            return
+        if len(tokens) < 1:
+            print("** class name missing **")
+            return
+        key = tokens[0] + "." + tokens[1]
+        if key in BaseModelStorage:
+            print()
+            return
+        else:
+            print("** no instance found **")
 
 
     def default(self, args):
