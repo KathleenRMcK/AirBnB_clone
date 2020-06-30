0aa88f74dc5229aea7c13736f421ab6a834fd524
Student <student@nhv-staff-02.local>
2020-06-30 14:01:03 -0400

    repush console with updated help for functions, reformatted structure for readability, added comments for testing running console

--- console.py
+++ console.py
@@ -11,15 +11,15 @@
 # Code should not be executed when imported
 
 import cmd
-import models
-from models.base_model import BaseModel
-from models.user import User
-from datetime import datetime
-from models.city import City
-from models.state import State
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+#import models
+#from models.base_model import BaseModel
+#from models.user import User
+#from datetime import datetime
+#from models.city import City
+#from models.state import State
+#from models.amenity import Amenity
+#from models.place import Place
+#from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -28,16 +28,45 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
 
-    def do_EOF(self, args):
-        """
-        EOF command exits out of the console
-        """
-        quit()
+    def do_quit(self, line):
+        """ Allows use of quit command to exit program """
+        return True
+
+    def do_EOF(self, line):
+        """ Allows use of EOF command to exit program """
+        return True
+
+    def emptyline(self):
+        """ Allows empty line + ENTER to provide blank space """
+        pass
+
     def do_help(self, args):
         """
         list all commands
         """
         cmd.Cmd.do_help(self, args)
+
+    """                                                                         
+    ***Help support documentation for all commands***                           
+    """
+    def help_create(self):
+        """ Documentation for the create command """
+        print 'Use of the create command creates a new instance of BaseModel, saves it to JSON file and prints the id with: create <class name>'
+    def help_show(self):
+        """ Documentation for the show command """
+        print 'Use of the show command retrieves an instance based on its ID with: <class name>.show(<id>)'
+    def help_count(self):
+        """ Documentation for the count command """
+        print 'Use of the count command retrieves the number of instances of a class with: <class name>.count()'
+    def help_destroy(self):
+        """ Documentation for the destroy command """
+        print 'Use of the destroy command destroys an instance based on its ID with: <class name>.destroy(<id>)'
+    def help_all(self):
+        """ Documentation for the all command """
+        print 'Use of the all command retrieves all instances of a class with: <class name>.all()'
+    def help_update(self):
+        """ Documentation for the update command """
+        print 'Use of the update command updates an instance based on its ID with: <class name>.update(<id>, <attribute name>, <attribute value>) and with a dictionary using: <class name>.update(<id>, <dictionary representation>)'
 
 
     def do_destroy(self, args):
@@ -155,19 +184,6 @@ class HBNBCommand(cmd.Cmd):
         default method to use with commands
         """
 
-
-    def do_quit(self, args):
-        """
-        exits out of the console
-        """
-        quit()
-
-
-    def emptyline(self):
-        """
-        prints prompt
-        """
-        return
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
