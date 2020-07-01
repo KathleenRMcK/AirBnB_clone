19acf7ec44d118180e5a818ad90eef31a413e1ca
dtvangogh <dt.van16@gmail.com>
2020-07-01 15:46:18 -0400

    use kwargs so print name, and my my number

--- models/base_model.py
+++ models/base_model.py
@@ -11,26 +11,26 @@ class BaseModel():
     """ Base Model Class """
     def __init__(self, *args, **kwargs):
         """ intializes id, created_at, and updated_at instance vars """
-        if len(args) > 0:
-            datetime_string = args[0]['created_at']
-            args[0]['created_at'] = (datetime.strptime(
-                datetime_string, "%Y-%m-%d %H:%M:%S.%f"))
-            update_datetime_string = args[0]['updated_at']
-            args[0]['updated_at'] = (datetime.strptime(
-                update_datetime_string, "%Y-%m-%d %H:%M:%S.%f"))
-            self.__dict__ = args[0]
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = (datetime.now())
+            self.updated_at = self.created_at = datetime.now()
             storage.new(self)
     def save(self):
         """ Saves current datetime to updated_at """
         self.updated_at = datetime.now()
         storage.save()
+
     def __str__(self):
         """ String representation ofinstance """
         return ("[{}] ({}) {}".format(type(self).__name__, self.id,
                                       self.__dict__))
+
     def to_dict(self):
         """ Returns dictionary """
         dictionary = {}
