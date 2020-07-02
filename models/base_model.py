ca80cdb8a61c2c8c33a9fa88c49db93a79d488fa
dtvangogh <dt.van16@gmail.com>
2020-07-02 13:15:31 -0400

    fixes line 13-18

--- models/base_model.py
+++ models/base_model.py
@@ -11,16 +11,9 @@ class BaseModel():
     """ Base Model Class """
     def __init__(self, *args, **kwargs):
         """ intializes id, created_at, and updated_at instance vars """
-        if kwargs:
-            for key, data in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, data)
-                if key == "created_at" or key == "updated_at":
-                    data = datetime.strptime(data, "%Y-%m-%dT%H:%M:%S.%f")
-        else:
-            self.id = str(uuid.uuid4())
-            self.updated_at = self.created_at = datetime.now()
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.updated_at = self.created_at = datetime.now()
+        storage.new(self)
 
     def save(self):
         """ Saves current datetime to updated_at """
